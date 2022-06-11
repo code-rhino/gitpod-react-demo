6099471e6744522152f6cc1577ad55d46bef1c43
Tariq Hook <tariq.hook@gmail.com>
2022-06-11 14:46:45 +0000

    updates

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicSelect from './components/BasicSelect';
+function App() {
+  return (
+    <div className="App">
+      <BasicSelect/>
+    </div>
+  );
+}
+
+export default App;
